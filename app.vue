8fb2dd5996b620aa75b28aed8ade056c25bee20f
Rita <shaidrv@gmail.com>
2023-06-01 15:46:01 +0400

    Вывела список отдаете

--- app.vue
+++ app.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="app__container">
-    <AppForm />
+    <AppGive :from-list="fromList" />
+    <!-- <AppGet /> -->
+    <!-- <AppForm /> -->
   </div>
 </template>
 
-<script></script>
+<script>
+import { defineComponent } from 'vue'
+import { DirectionsApi } from '@/api/api'
+
+export default defineComponent({
+  data() {
+    return {
+      fromList: [],
+      toList: [],
+    }
+  },
+
+  async mounted() {
+    const api = await DirectionsApi.getInstance()
+    this.fromList = await api.getFrom()
+    this.toList = await api.getTo(7)
+    // console.log(this.fromList)
+    // console.log(this.fromList)
+  },
+
+  methods: {},
+})
+</script>
 
 <style lang="scss">
 @import './assets/styles/main.scss';
--- components/AppGive.vue
+++ components/AppGive.vue
@@ -13,11 +13,18 @@
         </div>
       </div>
       <ul class="list__body">
-        <li class="list__item item" @click="$emit('selectTicker', ticker)">
-          <img src="item__img" alt="" />
+        <li
+          class="list__item item"
+          v-for="dir in fromList"
+          :key="dir.currency"
+          @click="handleClick(dir)"
+        >
+          <img class="item__img" alt="currency icon" :src="dir.logo.simple" />
           <div class="item__name">
-            <div class="item__title">BTC</div>
-            <div class="item__subtitle">BitCoin</div>
+            <div class="item__title">
+              {{ dir.currency.toString().toUpperCase() }}
+            </div>
+            <div class="item__subtitle">{{ dir.name }}</div>
           </div>
         </li>
       </ul>
@@ -26,12 +33,17 @@
 </template>
 
 <script>
+import { defineComponent } from 'vue'
+
 export default defineComponent({
-  // data() {
-  //   return {
-  //     selectedTicker: null,
-  //   }
-  // },
+  props: ['fromList'],
+  emits: ['selectDir'],
+  methods: {
+    handleClick(dir) {
+      this.$emit('selectDir', dir)
+      console.log(dir)
+    },
+  },
 })
 </script>
 
