da2a1650fc96f1ba203d2523ae96e8ddf9882e62
Rita <shaidrv@gmail.com>
2023-06-02 17:58:40 +0400

    подправила типы

--- types/form.ts
+++ types/form.ts
@@ -1,49 +1,49 @@
 interface DirectionDescription {
-    notify: string,
-    min: number,
-    max: number,
-    currency: string,
-    round: number,
-    monitoringCode: string
+  notify: string
+  min: number
+  max: number
+  currency: string
+  round: number
+  monitoringCode: string
 }
 
 interface Currency {
-    id: number,
-    currency: string,
-    active: boolean
+  id: number
+  currency: string
+  active: boolean
 }
 
 interface FormStructureImportPart {
-    header: string,
-    placeholder: string,
-    error: string,
-    visible: boolean
+  header: string
+  placeholder: string
+  error: string
+  visible: boolean
 }
 
 interface FormStructureInput {
-    type: string,
-    name: string,
-    send: FormStructureImportPart,
-    receive: FormStructureImportPart,
-    regex: string
+  type: string
+  name: string
+  send: FormStructureImportPart
+  receive: FormStructureImportPart
+  regex: string
 }
 
 interface FormStructureDirection {
-    currency: Currency[],
-    input: string[],
-    network: string[],
-    city: string[]
+  currency: Currency[]
+  input: FormStructureInput[]
+  network: string[]
+  city: string[]
 }
 
 interface FormStructure {
-    from: string,
-    to: string
+  from: FormStructureDirection
+  to: FormStructureDirection
 }
 export interface Form {
-    course: number,
-    bonus: number,
-    notify: string,
-    from: DirectionDescription,
-    to: DirectionDescription,
-    structure: FormStructure
+  course: number
+  bonus: number
+  notify: string
+  from: DirectionDescription
+  to: DirectionDescription
+  structure: FormStructure
 }
